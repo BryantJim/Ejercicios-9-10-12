5be80f2f24dc3ca6a45f4ee95ee7607aa240bc88
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-01-31 16:33:01 -0400

    Ejercicio 10.2

---
+++ Ejercicios 10/Ejercicio 10.1/Estudiantes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_10._1
+{
+    class Estudiantes
+    {
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public int OrdenLista { get; set; }
+        public String Telefono { get; set; }
+        public String Celular { get; set; }
+        public String Direccion { get; set; }
+
+        public Estudiantes()
+        {
+            Nombre = string.Empty;
+            Apellido = string.Empty;
+            OrdenLista = 0;
+            Telefono = string.Empty;
+            Celular = string.Empty;
+            Direccion = string.Empty;
+        }
+    }
+}
