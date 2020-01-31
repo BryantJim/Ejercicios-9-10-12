becc4c45f6b373a23c8e78c95307cfa66b6e5d30
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-01-31 12:05:06 -0400

    Ejercicio Cap 9

---
+++ EjercicioCap9.1/Ejercicio9.4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ejercicio9._4
+{
+    class Program
+    {
+        enum Neumaticos {Diagonal=1,Radial,Verano,Invierno,AllSeason,Asimetrico,Direccionales,Tubuless,
+        PerfilBajo,ReCauchutados,RunFlat,Ecologicos}
+
+        static void Main(string[] args)
+        {
+            Neumaticos neumaticos;
+            neumaticos = Neumaticos.Diagonal;
+            Console.WriteLine("Los neumaticos son: {0}",neumaticos);
+        }
+    }
+}
