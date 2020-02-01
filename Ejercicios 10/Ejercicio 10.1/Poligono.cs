d3ba839bcad53bb7e3822f25d9048ce2280bc031
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-02-01 09:56:46 -0400

    Ejercicio 10.3 Poligono

---
+++ Ejercicios 10/Ejercicio 10.1/Poligono.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Ejercicio_10._1
+{
+    public class Poligono
+    {
+        private int Lados;
+        private int Distancia;
+
+        public int lados
+        {
+            get
+            {
+                return Lados;
+            }
+            set
+            {
+                if (value >= 3)
+                    Lados = value;
+                else
+                    Lados = 0;
+            }
+        }
+
+        public int distancia
+        {
+            get
+            {
+                return Distancia;
+            }
+            set
+            {
+                if (value > 0)
+                    Distancia = value;
+                else
+                    Distancia = 0;
+            }
+        }
+        public Poligono()
+        {
+            Lados = 0;
+            Distancia = 0;
+        }
+
+        public Poligono(int lados, int distancia)
+        {
+            Lados = lados;
+            Distancia = distancia;
+        }
+
+        public override string ToString()
+        {
+            String mensaje = "";
+            mensaje += "Los lados del poligono son " + Lados.ToString() +
+                        " y su distancia es " + Distancia.ToString();
+            return mensaje;
+        }
+    }
+}
