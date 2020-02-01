1ed89d107e32e7a49dee5966cbb5c122f862a457
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-02-01 10:31:27 -0400

    Ejercicios cap 12

---
+++ Ejercicio 12/Ejercicio 12.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio_12_1
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            int Numero;
+
+            Console.Write("Ingrese un numero: ");
+            Numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("El factorial de {0} es {1}", Numero, factorial(Numero));
+        }
+
+        public static int factorial(int Num)
+        {
+            int Resultado = 1;
+
+            for (int i = Num; i > 1; i--)
+            {
+                Resultado *= i;
+            }
+
+            return Resultado;
+        }
+    }
+}
